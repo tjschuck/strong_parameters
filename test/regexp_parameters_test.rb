929332d0f2a6528c1300209e624930ad1ec3e438
T.J. Schuck <tj@getharvest.com>
2012-05-14 14:28:31 -0400

    Revert to 1.8 hash syntax
    ec7b957bf113240d8cc3ed2017d9866e83e2edf7 made strong_parameters 1.8 compatible

--- test/regexp_parameters_test.rb
+++ test/regexp_parameters_test.rb
@@ -4,29 +4,29 @@ require 'action_controller/parameters'
 class RegexpParametersTest < ActiveSupport::TestCase
   test "permitted parameters defined by regexp" do
     params = ActionController::Parameters.new({
-      object_1: {
-        safe_param_1: 'value',
-        safe_param_2: 'value',
-        malicious: 'haxored!'
+      :object_1 => {
+        :safe_param_1 => 'value',
+        :safe_param_2 => 'value',
+        :malicious => 'haxored!'
       },
-      object_2: {
-        safe_param_1: 'value',
-        safe_param_2: 'value',
-        malicious: 'haxored!'
+      :object_2 => {
+        :safe_param_1 => 'value',
+        :safe_param_2 => 'value',
+        :malicious => 'haxored!'
       },
-      bad_object: {
-        param: 'value'
+      :bad_object => {
+        :param => 'value'
       }
     })
 
     expected = ActionController::Parameters.new({
-      object_1: {
-        safe_param_1: 'value',
-        safe_param_2: 'value'
+      :object_1 => {
+        :safe_param_1 => 'value',
+        :safe_param_2 => 'value'
       },
-      object_2: {
-        safe_param_1: 'value',
-        safe_param_2: 'value'
+      :object_2 => {
+        :safe_param_1 => 'value',
+        :safe_param_2 => 'value'
       }
     })
 
